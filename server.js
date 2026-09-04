faec8f17aa3056170e11d1f669be35b7acbff773
MarianaSorochynskas <S01361511@acad.tri-c.edu>
2026-09-03 21:25:50 -0400

    Added home page and nodemon package

--- server.js
+++ server.js
@@ -2,7 +2,25 @@ import http from 'http';
 
 const server = http.createServer((request, response) => {
     console.log(`${request.method} ${request.url}`);
-    response.write('Hello World');
+
+    if (request.url === '/') {
+        response.writeHead(200, { 'Content-Type': 'text/html' });
+
+        response.write(`
+            <html>
+                <body>
+                    <h1>My first Web Server</h1>
+                    <p>Mariana Sorochynska</p>
+
+                    <a href="/about">About</a><br>
+                    <a href="/time">Time</a><br>
+                    <a href="/greet">Greetings</a>
+                </body>
+            </html
+        `);
+
+        response.end();
+    }
     response.end();
     
 });
